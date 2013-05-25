b3928e8371002707b6d5d404c0b6b6361da77f16
Ernie Miller <ernie@erniemiller.org>
2013-05-25 18:08:01 -0400

    Alias base_klass to active_record on JoinBase.
    Release 0.6.3

--- lib/polyamorous.rb
+++ lib/polyamorous.rb
@@ -5,9 +5,11 @@ module Polyamorous
   if defined?(::ActiveRecord::Associations::JoinDependency)
     JoinDependency  = ::ActiveRecord::Associations::JoinDependency
     JoinAssociation = ::ActiveRecord::Associations::JoinDependency::JoinAssociation
+    JoinBase = ::ActiveRecord::Associations::JoinDependency::JoinBase
   else
     JoinDependency  = ::ActiveRecord::Associations::ClassMethods::JoinDependency
     JoinAssociation = ::ActiveRecord::Associations::ClassMethods::JoinDependency::JoinAssociation
+    JoinBase = ::ActiveRecord::Associations::ClassMethods::JoinDependency::JoinBase
   end
 end
 
@@ -17,3 +19,8 @@ require 'polyamorous/join_dependency'
 
 Polyamorous::JoinDependency.send(:include, Polyamorous::JoinDependencyExtensions)
 Polyamorous::JoinAssociation.send(:include, Polyamorous::JoinAssociationExtensions)
+Polyamorous::JoinBase.class_eval do
+  if method_defined?(:active_record)
+    alias_method :base_klass, :active_record
+  end
+end
