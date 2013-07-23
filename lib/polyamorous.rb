b61dda8fa6bc78362c1d2f7686e8322eddd409de
Johnny Shields <johnny.shields@gmail.com>
2013-07-23 10:56:55 +0900

    Do not require ActiveRecord

--- lib/polyamorous.rb
+++ lib/polyamorous.rb
@@ -1,26 +1,27 @@
 require "polyamorous/version"
-require 'active_record'
 
-module Polyamorous
-  if defined?(::ActiveRecord::Associations::JoinDependency)
-    JoinDependency  = ::ActiveRecord::Associations::JoinDependency
-    JoinAssociation = ::ActiveRecord::Associations::JoinDependency::JoinAssociation
-    JoinBase = ::ActiveRecord::Associations::JoinDependency::JoinBase
-  else
-    JoinDependency  = ::ActiveRecord::Associations::ClassMethods::JoinDependency
-    JoinAssociation = ::ActiveRecord::Associations::ClassMethods::JoinDependency::JoinAssociation
-    JoinBase = ::ActiveRecord::Associations::ClassMethods::JoinDependency::JoinBase
+if defined?(::ActiveRecord)
+  module Polyamorous
+    if defined?(::ActiveRecord::Associations::JoinDependency)
+      JoinDependency  = ::ActiveRecord::Associations::JoinDependency
+      JoinAssociation = ::ActiveRecord::Associations::JoinDependency::JoinAssociation
+      JoinBase = ::ActiveRecord::Associations::JoinDependency::JoinBase
+    else
+      JoinDependency  = ::ActiveRecord::Associations::ClassMethods::JoinDependency
+      JoinAssociation = ::ActiveRecord::Associations::ClassMethods::JoinDependency::JoinAssociation
+      JoinBase = ::ActiveRecord::Associations::ClassMethods::JoinDependency::JoinBase
+    end
   end
-end
 
-require 'polyamorous/join'
-require 'polyamorous/join_association'
-require 'polyamorous/join_dependency'
+  require 'polyamorous/join'
+  require 'polyamorous/join_association'
+  require 'polyamorous/join_dependency'
 
-Polyamorous::JoinDependency.send(:include, Polyamorous::JoinDependencyExtensions)
-Polyamorous::JoinAssociation.send(:include, Polyamorous::JoinAssociationExtensions)
-Polyamorous::JoinBase.class_eval do
-  if method_defined?(:active_record)
-    alias_method :base_klass, :active_record
+  Polyamorous::JoinDependency.send(:include, Polyamorous::JoinDependencyExtensions)
+  Polyamorous::JoinAssociation.send(:include, Polyamorous::JoinAssociationExtensions)
+  Polyamorous::JoinBase.class_eval do
+    if method_defined?(:active_record)
+      alias_method :base_klass, :active_record
+    end
   end
 end
