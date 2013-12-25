2e5f4056483d07b38ac29887fc8aa28ef7e5ce32
Washington Luiz <huoxito@gmail.com>
2013-12-25 14:14:06 -0300

    Remove a couple more deprecated JoinDependency api
    Looking into the history for ActiveRecord::Associations::JoinDependency
    should give us some help on why each one of those were removed. See for
    example:
    
    https://github.com/rails/rails/commit/868457488d3c43ff4e9c28e34883620d90345185

--- lib/polyamorous/join_dependency.rb
+++ lib/polyamorous/join_dependency.rb
@@ -10,39 +10,24 @@ module Polyamorous
       end
     end
 
-    if ActiveRecord::VERSION::STRING =~ /^3\.0\./
-      def _join_parts
-        @joins
-      end
-    else
-      def _join_parts
-        @join_parts
-      end
-    end
-
-    def build_with_polymorphism(associations, parent = nil)
+    def build_with_polymorphism(associations, parent = join_root.to_a.last)
       case associations
       when Join
-        parent ||= _join_parts.last
-        reflection = parent.reflections[associations.name] or
+        reflection = parent.base_klass.reflections[associations.name] or
           raise ::ActiveRecord::ConfigurationError, "Association named '#{ associations.name }' was not found; perhaps you misspelled it?"
 
         unless join_association = find_join_association_respecting_polymorphism(reflection, parent, associations.klass)
-          @reflections << reflection
           join_association = build_join_association_respecting_polymorphism(reflection, parent, associations.klass)
-          join_association.join_type = associations.type
-          _join_parts << join_association
-          cache_joined_association(join_association)
         end
 
         join_association
       else
-        build_without_polymorphism(associations, parent)
+        build_without_polymorphism(associations, parent = join_root.to_a.last)
       end
     end
 
     def find_join_association_respecting_polymorphism(reflection, parent, klass)
-      if association = find_join_association(reflection, parent)
+      if association = parent.children.find { |j| j.reflection == reflection }
         unless reflection.options[:polymorphic]
           association
         else
