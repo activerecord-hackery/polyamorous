45db82302047a3ad1562d8757e4eddfaaeb018dd
Jon Atack <jonnyatack@gmail.com>
2015-03-29 10:38:19 +0530

    `silence_stream` is not thread-safe
    and deprecated in Rails 4.2.1 and 5.
    
    This was also causing the Travis CI tests to fail with Rails master.
    
    Replace with `ActiveRecord::Migration.verbose = false`

--- spec/support/schema.rb
+++ spec/support/schema.rb
@@ -34,45 +34,43 @@ end
 
 module Schema
   def self.create
-    silence_stream(STDOUT) do
+    ActiveRecord::Migration.verbose = false
 
-      ActiveRecord::Schema.define do
-        create_table :people, force: true do |t|
-          t.integer  :parent_id
-          t.string   :name
-          t.integer  :salary
-          t.boolean  :awesome, default: false
-          t.timestamps null: false
-        end
-
-        create_table :articles, force: true do |t|
-          t.integer :person_id
-          t.string  :title
-          t.text    :body
-        end
-
-        create_table :comments, force: true do |t|
-          t.integer :article_id
-          t.integer :person_id
-          t.text    :body
-        end
-
-        create_table :tags, force: true do |t|
-          t.string :name
-        end
-
-        create_table :articles_tags, force: true, id: false do |t|
-          t.integer :article_id
-          t.integer :tag_id
-        end
-
-        create_table :notes, force: true do |t|
-          t.integer :notable_id
-          t.string  :notable_type
-          t.string  :note
-        end
+    ActiveRecord::Schema.define do
+      create_table :people, force: true do |t|
+        t.integer  :parent_id
+        t.string   :name
+        t.integer  :salary
+        t.boolean  :awesome, default: false
+        t.timestamps null: false
       end
 
+      create_table :articles, force: true do |t|
+        t.integer :person_id
+        t.string  :title
+        t.text    :body
+      end
+
+      create_table :comments, force: true do |t|
+        t.integer :article_id
+        t.integer :person_id
+        t.text    :body
+      end
+
+      create_table :tags, force: true do |t|
+        t.string :name
+      end
+
+      create_table :articles_tags, force: true, id: false do |t|
+        t.integer :article_id
+        t.integer :tag_id
+      end
+
+      create_table :notes, force: true do |t|
+        t.integer :notable_id
+        t.string  :notable_type
+        t.string  :note
+      end
     end
 
     10.times do
